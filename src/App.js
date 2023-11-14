9eaf4b6a0a158d6d4a0fdf23e0a50d9d03c689af
Jorge Caza <jorgecaza@Jorges-MacBook-Pro.local>
2023-11-14 22:44:09 +0000

    main js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import SearchBar from './components/SearchBar';
+import SearchResults from './components/SearchResults';
+import { useState,useEffect } from 'react';
+import './css/App.css';
+import './css/searchBar.css';
+import {Saveplaylist} from './components/Playlistactions';
+import './css/track.css';
+import TrackList from './components/TrackList';
+import Playlist from './components/Playlist';
 
 function App() {
+const [results, setResults]=useState([]);
+const [songs,addSongs]=useState([]);
+const [playlistName,setPlaylistName]= useState('');
+const [accessToken, setAccessToken]=useState('');
+
+const addSongstoPlaylist= (song) =>{
+
+   const arr = songs.filter((r) => r.id === song.id);
+   if (!arr.length>0){
+  addSongs((songs) => [song, ...songs])
+  }
+}
+const removeFromPlaylist =(result) =>{
+  
+  addSongs(songs.filter((r) => r.id !== result.id));
+}
+
+const namingPlaylist =(text)=>{
+   setPlaylistName(text)
+  // console.log('Dentro: '+ text);
+}
+
+const saveplaylist =(songslist) => {
+  //console.log('Dentro: '+ playlistName);
+  //console.log(songslist);
+  //console.log(accessToken);
+  Saveplaylist(songslist, playlistName, accessToken);
+ // console.log('Dentro: '+ text);
+}
+
+
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <>
+  <div className='headerTop'>
+  <h1>Musica -- Your Favorite Music</h1>
+  </div>
+  <div className='searchDiv'>
+  <SearchBar setResults={setResults} accessToken={accessToken} setAccessToken={setAccessToken}/>
+  </div>
+  <div className='searchDivS'> 
+  <TrackList results={results} addSongstoPlaylist={addSongstoPlaylist} removeFromPlaylist={removeFromPlaylist}/>
+  </div>
+  
+  <Playlist songs={songs} playlistName={playlistName} removeFromPlaylist={removeFromPlaylist} namingPlaylist={namingPlaylist} saveplaylist={saveplaylist}/>
+  
+  
+  </>
   );
 }
 
